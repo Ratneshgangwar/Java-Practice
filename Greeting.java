0b6af33959c080760e9d27edf90bca6715dca873
Ratneshgangwar <2023555662.ratnesh@sharda.ac.in>
2026-02-03 12:08:09 +0530

    Simple Interest

--- Greeting.java
+++ Greeting.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 class Greeting {
   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter your name :");
-    String name=sc.nextLine();
-    System.out.println("Hello "+name+", welcome to Java Programming!");
+    try (Scanner sc=new Scanner(System.in)) {
+      System.out.println("Enter your name :");
+      String name=sc.nextLine();
+      System.out.println("Hello "+name+", welcome to Java Programming!");
+    }
   }
 }
