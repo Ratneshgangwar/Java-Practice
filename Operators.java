fec2e08a4466dfbfebfc6174a10e83d0e15615cc
Ratneshgangwar <2023555662.ratnesh@sharda.ac.in>
2026-02-03 14:41:45 +0530

    Fibonacci Number

--- Operators.java
+++ Operators.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Operators {
   public static void main(String [] args){
-    Scanner sc =new Scanner(System.in);
-    System.out.println("Enter First number:");
-    int xx=sc.nextInt();
-    System.out.println("Enter Second number:");
-    int yy=sc.nextInt();
-    System.out.println("Addition : "+(xx+yy));
-    System.out.println("Subtraction : "+(xx-yy)); 
-    System.out.println("Multiplication : "+(xx*yy));
-    System.out.println("Division : "+(xx/yy));  
-
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.println("Enter First number:");
+      int xx=sc.nextInt();
+      System.out.println("Enter Second number:");
+      int yy=sc.nextInt();
+      System.out.println("Addition : "+(xx+yy));
+      System.out.println("Subtraction : "+(xx-yy)); 
+      System.out.println("Multiplication : "+(xx*yy));
+      System.out.println("Division : "+(xx/yy));
+    }
   }
 }
